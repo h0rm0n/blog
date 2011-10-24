13c3a788d336e9f570c2301a66141db5e70360e5
Łukasz Horonziak <hormon2343@gmail.com>
2011-10-24 22:22:29 +0200

    small fix to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Blog::Application.routes.draw do
       resources :comments
    end
 
-  # root :to => "posts#index"
+  root :to => "posts#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
